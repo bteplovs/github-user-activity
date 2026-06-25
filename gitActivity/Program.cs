48bde3d64ec0dfa06d18a37cdc5e3a3ebd4d3475
bte32 <bteplovs@gmail.com>
2026-06-25 20:53:56 +1200

    feat: argument parsing

---
+++ gitActivity/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace gitActivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("One argument required: dotnet run -- <username> ");
+                return;
+            }
+
+            var username = args[0];
+
+            
+
+
+        }
+    }
+}
